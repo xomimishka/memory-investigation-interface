8bc0817f1231fb80e2a3ed0223400b4c7396015f
xomimishka <sklyarovamilenav@gmail.com>
2026-07-02 18:14:04 +0300

    Изучение Go и API

--- main.go
+++ main.go
@@ -6,13 +6,26 @@ import (
 )
 
 func main() {
+	IMT := calculateIMT(getUserInput())
+	outputresult(IMT)
+}
+
+func outputresult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
+	fmt.Print(result)
+}
+
+func calculateIMT(USERkG float64, USERheight float64) float64 {
 	const IMTPower = 2
+	return USERkG / math.Pow(USERheight / 100, IMTPower)
+}
+
+func getUserInput() (float64, float64) {
 	var height, kg float64
-	fmt.Print("Калькулятор индекса тела \n")
-	fmt.Print("Введите рост в метрах: ")
+	fmt.Println("Калькулятор индекса тела")
+	fmt.Print("Введите рост в сантиметрах: ")
 	fmt.Scan(&height)
 	fmt.Print("Введите вес в килограммах: ")
 	fmt.Scan(&kg)
-	IMT := kg / math.Pow(height, IMTPower)
-	fmt.Print("Ваш индекс массы тела: ", IMT)
+	return kg, height
 }
